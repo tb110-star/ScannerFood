72cb3b96ecc99cacaa1db847e29ca5f1860faef4
Tarlan Bakhtiari <64501123+tb110-star@users.noreply.github.com>
2025-02-21 18:01:35 +0100

    last update

--- FoodScanner/Services/FireStorerManager.swift
+++ FoodScanner/Services/FireStorerManager.swift
@@ -15,7 +15,10 @@ final class FireStoreManeger {
         Firestore.firestore().collection("Image detection")
     }
     
-    func insertHistory(nutritionData: NutritionResponse,recognizedItems:[SelectedIngredient],imageUrl: String, timestamp: Date = Date()) async throws {
+    func insertHistory(nutritionData: NutritionResponse,recognizedItems:[SelectedIngredient],imageUrl: String, timestamp: Date = Date()) async throws (Error){
+        guard let creatorID = await AuthManager.shared.userID else {
+            throw .notAuthenticated
+        }
           let historyEntry: [String: Any] = [
               "timestamp": Timestamp(date: timestamp),
               "NutritionData": nutritionData,
@@ -23,14 +26,42 @@ final class FireStoreManeger {
               "imageUrl": imageUrl
               
           ]
-          
+        let historyDoc =  HistoryModel(timestamp: <#T##Date#>, finalIngredients: <#T##[SelectedIngredient]#>, nutritionData: <#T##NutritionResponse#>, isFavorite: <#T##Bool#>, imageUrl: <#T##String#>)
           do {
               try await db.addDocument(data: historyEntry)
               print("✅ Successfully saved recognized food items to Firestore!")
           } catch {
               print("❌ Error saving recognized food items: \(error.localizedDescription)")
-              throw error
+              throw .failedDeleting(reason: error.localizedDescription)
           }
       }
-   
+  
+    enum Error: LocalizedError {
+        case notAuthenticated
+        case noSnippetID
+        case failedCreation(reason: String)
+        case failedFinding(reason: String)
+        case failedDeleting(reason: String)
+        case failedUpdating(reason: String)
+        case findAllFailed(reason: String)
+        
+        var errorDescription: String? {
+            switch self {
+            case .notAuthenticated:
+                "You are not authenticated."
+            case .noSnippetID:
+                "The snippet does not exist yet."
+            case .failedCreation(let reason):
+                "The snippet could not be created: \(reason)"
+            case .failedFinding(let reason):
+                "The snippet could not be found: \(reason)"
+            case .failedDeleting(let reason):
+                "The snippet could not be deleted: \(reason)"
+            case .failedUpdating(let reason):
+                "The snippet could not be updated: \(reason)"
+            case .findAllFailed(let reason):
+                "Your snippet could not be loaded: \(reason)"
+            }
+        }
+    }
 }
